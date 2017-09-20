4a933a8868d7bbb1b24218004f1d6ae6bcee486a
Kevin Elledge <kelledge@petropower.com>
2017-09-20 13:36:27 -0500

    Add db instance
    First pass at non-configurable DB instance. This is mostly in an attempt to
    find which fields should be configurable for a user.

--- terraform/rds/db.tf
+++ terraform/rds/db.tf
@@ -6,11 +6,48 @@ variable "route_table_id" {
   description = "The routing table to use with the database subnets"
 }
 
-/*
 output "db" {
   value = "${aws_db_instance.default.endpoint}"
 }
+
+/*
+*  DatabaseConnections Count
+*  CPUUtilization %
+*  FreeStorageSpace Bytes
+*  FreeableMemory Bytes
+*
+* endpoint
+* port
 */
+resource "aws_db_instance" "default" {
+  identifier                = "db1-primary"
+  instance_class            = "db.t2.micro"
+
+  allocated_storage         = 100
+  storage_type              = "gp2"
+
+  engine                    = "postgres"
+  engine_version            = "9.6.2"
+  parameter_group_name      = "default.postgres9.6"
+
+  username                  = "root_user"
+  password                  = "ZdOjYQyw*%98"
+
+  backup_retention_period   = 7
+  backup_window             = "08:00-08:30"         /* 03:00-03:30 CDT */
+  maintenance_window        = "Sun:06:00-Sun:06:30" /* Sun:01:00-Sun:01:30 CDT */
+
+  skip_final_snapshot       = false
+  final_snapshot_identifier = "db1-primary-final"
+
+  publicly_accessible  = false
+  vpc_security_group_ids = []
+  db_subnet_group_name = "${aws_db_subnet_group.default.name}"
+
+  tags = {
+    Name = "main"
+  }
+}
 
 /*
 *
