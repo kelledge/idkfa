51a1870db9f36452e08490e589922b1e1a174fe2
Kevin Elledge <kelledge@petropower.com>
2017-09-21 18:54:01 -0500

    IAM first pass
    Supports adding a single user, with console login and API access keys

--- terraform/iam/kelledge.tf
+++ terraform/iam/kelledge.tf
@@ -1,4 +1,27 @@
 resource "aws_iam_user" "kelledge" {
-  name = "kelledge"
+  name = "ZZZ-kelledge"
   path = "/"
+  force_destroy = true
+}
+
+resource "aws_iam_user_login_profile" "kelledge" {
+  user    = "${aws_iam_user.kelledge.name}"
+  pgp_key = "${file("${path.module}/kelledge.gpg.pub")}"
+}
+
+resource "aws_iam_access_key" "kelledge" {
+  user    = "${aws_iam_user.kelledge.name}"
+  pgp_key = "${file("${path.module}/kelledge.gpg.pub")}"
+}
+
+output "password" {
+  value = "${aws_iam_user_login_profile.kelledge.encrypted_password}"
+}
+
+output "secret" {
+  value = "${aws_iam_access_key.kelledge.encrypted_secret}"
+}
+
+output "key_fingerprint" {
+  value = "${aws_iam_access_key.kelledge.key_fingerprint}"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,36 @@
+module "iam" {
+  source = "./iam"
+}
+
+output "password" {
+  value = "${module.iam.password}"
+}
+
+output "secret" {
+  value = "${module.iam.secret}"
+}
+
+output "key_fingerprint" {
+  value = "${module.iam.key_fingerprint}"
+}
+
+
+/*
 module "vpc" {
   source = "./vpc"
 }
+*/
 
+/*
+module "rds" {
+  source         = "./rds"
+
+  vpc_id         = "${module.vpc.vpc_id}"
+  route_table_id = "${module.vpc.route_table_private_id}"
+}
+*/
+
+/*
 output "vpc_id" {
   value = "${module.vpc.vpc_id}"
 }
@@ -9,11 +38,4 @@ output "vpc_id" {
 output "route_table_private_id" {
   value = "${module.vpc.route_table_private_id}"
 }
-
-
-module "rds" {
-  source         = "./rds"
-
-  vpc_id         = "${module.vpc.vpc_id}"
-  route_table_id = "${module.vpc.route_table_private_id}"
-}
+*/
