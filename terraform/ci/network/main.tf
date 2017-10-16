ce4062db721e0569a2a082b6826a2c5f719a2550
Kevin Elledge <kevin.elledge@gmail.com>
2017-10-15 22:57:25 -0500

    Add network to ci

--- terraform/ci/network/main.tf
+++ terraform/ci/network/main.tf
@@ -15,13 +15,20 @@ module "network" {
 
   name               = "network"
   cidr               = "10.0.0.0/16"
-  external_subnets   = ["10.0.0.0/24"]
-  internal_subnets   = ["10.0.1.0/24"]
+  external_subnets   = ["10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19"]
+  internal_subnets   = ["10.0.128.0/19", "10.0.160.0/19", "10.0.192.0/19"]
   environment        = "ci"
-  availability_zones = ["us-east-1a"]
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
+// FIXME: For some reason, this always dereferences to the current date ...
 output "id" {
+  value = "${module.network.id}"
+}
+
+// NOTE: Added as work around to the above FIXME. Probably will drop id in favor
+// of vpc_id anyway. It is more clear.
+output "vpc_id" {
   value = "${module.network.id}"
 }
 
@@ -53,4 +60,16 @@ output "internal_rtb_id" {
 // The external route table ID.
 output "external_rtb_id" {
   value = "${module.network.external_rtb_id}"
+}
+
+output "intranet_sg_id" {
+  value = "${aws_security_group.intranet.id}"
+}
+
+output "ssh_sg_id" {
+  value = "${aws_security_group.ssh.id}"
+}
+
+output "weg_sg_id" {
+  value = "${aws_security_group.web.id}"
 }
---
+++ terraform/ci/network/security-groups.tf
@@ -0,0 +1,78 @@
+resource "aws_security_group" "intranet" {
+  name        = "intranet"
+  description = "Intranet access"
+  vpc_id      = "${module.network.id}"
+
+  tags = {
+    Name = "intranet"
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ssh" {
+  name        = "ssh"
+  description = "SSH access"
+  vpc_id      = "${module.network.id}"
+
+  tags = {
+    Name = "ssh"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "web" {
+  name        = "web"
+  description = "Web access"
+  vpc_id      = "${module.network.id}"
+
+  tags = {
+    Name = "web"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
