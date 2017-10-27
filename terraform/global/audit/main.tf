6fb0f56e3739b84e25b0e45514550ece710b84f6
Kevin Elledge <kevin.elledge@gmail.com>
2017-10-26 22:36:04 -0500

    Expose new module variables
    Output the CloudWatch Log Group name to the remote state.

--- terraform/global/audit/main.tf
+++ terraform/global/audit/main.tf
@@ -7,9 +7,17 @@ terraform {
 }
 
 module "audit" {
-  source              = "../../modules/cloudtrail"
+  source = "../../modules/cloudtrail"
 
   s3_bucket_name      = "audit.kaak.us"
   s3_key_prefix       = "cloudtrail/"
-  data_retention_days = 2
+  data_retention_days = 3
+}
+
+output "cloudtrail_arn" {
+  value = "${module.audit.cloudtrail_arn}"
+}
+
+output "cloudwatch_log_group_name" {
+  value = "${module.audit.cloudwatch_log_group_name}"
 }
