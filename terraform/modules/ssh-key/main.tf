b4994f20a413a9ca52c525974958f36d344558cd
Kevin Elledge <kevin.elledge@gmail.com>
2017-11-26 10:44:30 -0600

    Add ssh-keygen module
    Creates local keypair and AWS EC2 keypair

---
+++ terraform/modules/ssh-key/main.tf
@@ -0,0 +1,42 @@
+variable "key_name" {
+  description = "Name to identify the key after creation"
+}
+
+variable "key_path" {
+  description = "Filesystem path to use when generating the key"
+}
+
+locals {
+  key_path_sec = "${var.key_path}"
+  key_path_pub = "${var.key_path}.pub"
+}
+
+resource "null_resource" "key" {
+  # Trigger on cluster id
+  # triggers {
+  #   cluster_instance_ids = "${join(",", aws_instance.cluster.*.id)}"
+  # }
+
+  provisioner "local-exec" {
+    # provision SSH key
+    command = "ssh-keygen -t rsa -b 2048 -C '${var.key_name}' -f ${local.key_path_sec}"
+  }
+}
+
+resource "aws_key_pair" "key" {
+  key_name = "${var.key_name}"
+  public_key = "${file(local.key_path_pub)}"
+  depends_on = ["null_resource.key"]
+}
+
+output "key_name" {
+  value = "${aws_key_pair.key.key_name}"
+}
+
+output "key_path_sec" {
+  value = "${local.key_path_sec}"
+}
+
+output "key_path_pub" {
+  value = "${local.key_path_pub}"
+}
