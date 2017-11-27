4944f1ad488cc86b71e0d5387c49b694554baa6a
Kevin Elledge <kevin.elledge@gmail.com>
2017-11-26 19:11:24 -0600

    Rename to "echo" service

--- terraform/ci/services/echo/main.tf
+++ terraform/ci/services/echo/main.tf
@@ -40,40 +40,42 @@ data "terraform_remote_state" "lb" {
   }
 }
 
+data "terraform_remote_state" "dns" {
+  backend = "s3"
+
+  config {
+    bucket = "state.kaak.us"
+    key    = "terraform/ci/dns.tfstate"
+    region = "us-east-1"
+  }
+}
+
 /*
 * ALB
 */
-resource "aws_lb_target_group" "test_service" {
-  name     = "test-tg"
+resource "aws_lb_target_group" "echo" {
+  name     = "echo-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = "${data.terraform_remote_state.network.vpc_id}"
 }
 
-resource "aws_lb_listener_rule" "test_service" {
-  listener_arn = "${data.terraform_remote_state.lb.http_listener_arn}"
+resource "aws_lb_listener_rule" "echo" {
+  listener_arn = "${data.terraform_remote_state.lb.https_listener_arn}"
   priority     = 99
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.test_service.arn}"
+    target_group_arn = "${aws_lb_target_group.echo.arn}"
   }
 
   condition {
     field  = "host-header"
-    values = ["test-service.kaak.us"]
+    values = ["echo.ci.kevinelledge.com"]
   }
 }
 
-/*data "terraform_remote_state" "lb" {
-  backend = "s3"
-
-  config {
-    bucket = "state.kaak.us"
-    key    = "terraform/ci/lb.tfstate"
-    region = "us-east-1"
-  }
-}
+/*
 * ECS
 */
 resource "aws_ecs_task_definition" "echo" {
@@ -112,7 +114,7 @@ resource "aws_ecs_service" "echo" {
   # }
 
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.test_service.arn}"
+    target_group_arn = "${aws_lb_target_group.echo.arn}"
     container_name = "echo"
     container_port = 8080
   }
@@ -121,4 +123,16 @@ resource "aws_ecs_service" "echo" {
   #   type       = "memberOf"
   #   expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
   # }
+}
+
+resource "aws_route53_record" "echo" {
+  zone_id = "${data.terraform_remote_state.dns.zone_id}"
+  name    = "echo.ci.kevinelledge.com"
+  type    = "A"
+
+  alias {
+    name                   = "${data.terraform_remote_state.lb.dns_name}"
+    zone_id                = "${data.terraform_remote_state.lb.zone_id}"
+    evaluate_target_health = true
+  }
 }
